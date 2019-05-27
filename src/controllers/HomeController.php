9c86b217c05a9dd9a3d0449b363f616ec3bdde65
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2019-05-27 15:50:06 -0300

    Create the initials commands

---
+++ src/controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * This class was be create automatically from `Teed Maker Cli`!
+ * You can see how here:
+ * https://github.com/teedmaker/cli
+ *
+ **/
+
+class HomeController
+{
+  public function home()
+  {
+    return $this->view('home.index');
+  }
+}
+
